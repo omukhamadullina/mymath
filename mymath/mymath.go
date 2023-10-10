0b0e51a9b372461f0f3d0c107e8dddaeeee42926
Olga-Muhamadullina <142241700+Olga-Muhamadullina@users.noreply.github.com>
2023-10-10 11:12:30 +0300

    Add files via upload

---
+++ mymath/mymath.go
@@ -0,0 +1,16 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) int {
+	return int(math.Sqrt(x))
+}
+func Abs(x float64) int {
+	return int(math.Abs(x))
+}
+func Max(x, y float64) int {
+	return int(math.Max(x, y))
+}
+func Yn(n int, x float64) int {
+	return int(math.Yn(n, x))
+}
